ac4a9b97cbc5c6b118bf057f81f35c34b519b257
Joël S. <20297204+joelsrf@users.noreply.github.com>
2025-03-30 10:26:21 +0200

    Update settings.py - titel

--- backend/settings.py
+++ backend/settings.py
@@ -45,7 +45,7 @@ class _UiSettings(BaseSettings):
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Losschwätze!"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_description: str = "Dieser Chat hilft dir, AI bei SRF anzuwenden. Er kennt alle Richtlinien und Tools und hilft mir praktischne Tipps!"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
